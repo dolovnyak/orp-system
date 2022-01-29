241d413a97e43fae043483b0832b8cc423194555
dolovnyak <dolovnyak@yandex.ru>
2022-01-29 23:02:35 +0300

    deleted unused class

--- ProcessesRunner.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Graph.hpp"
-
-class ProcessesRunner {
-public:
-    static void RunOptimalProcesses(Graph& graph, std::list<Process>& running_processes,
-                                    size_t remaining_cycle, size_t current_cycle);
-};
